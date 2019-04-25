00b080a12409c4df67e85d28933e6d555583f147
RealMetamorph <andr.timchuk@yandex.ru>
2019-04-26 00:34:03 +0300

    some stiles

--- blog/articles/static/js/highlight-post.js
+++ blog/articles/static/js/highlight-post.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
     $('.one-post').hover(function (event) {
             $(event.currentTarget).find('.one-post-shadow').animate({opacity: '0.3'}, 300);
-            $(event.currentTarget).animate({color: '#efff00'}, 300);
+            $(event.currentTarget).animate({color: '#d7fdff'}, 300);
         },
         function (event) {
             $(event.currentTarget).find('.one-post-shadow').animate({opacity: '0'}, 300);
             $(event.currentTarget).animate({color: '#0082ff'}, 300);
-
         });
     $('#logo').hover(function (event) {
-            $(event.currentTarget).animate({width: '30%'}, 300);
+            $(event.currentTarget).animate({width: '35%'}, 300);
         },
         function (event) {
-            $(event.currentTarget).animate({width: '15%'}, 300);
+            $(event.currentTarget).animate({width: '20%'}, 300);
         });
 });
